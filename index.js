f0aebf7a9a1d38f73f67de5b43940e9fdf2c6fdc
Kas <kas@Osono.local>
2020-02-14 22:18:58 +0000

    Discovered pulse oscillators

--- index.js
+++ index.js
@@ -88,7 +88,7 @@ class Baseline {
 
   toggleSound(type, input) {
     let method = type === 144 ? 'triggerAttack' : 'triggerRelease';
-    console.log('input : note', input, mapMIDINumberToTone(input))
+    //console.log('input : note', input, mapMIDINumberToTone(input))
     this.synth[method](mapMIDINumberToTone(input));
     this.synth2[method](mapMIDINumberToTone(input));
   }
@@ -116,55 +116,46 @@ class Baseline {
 
 class Chimes {
   constructor() {
-    this.synth = new Tone.PolySynth(4, Tone.Synth, {
-      volume: 1,
+    this.attack = 36 / 1000
+    this.decay = 46 / 1000
+    this.sustain = 63 / 127 
+    this.release = 42 / 100
+  
+    this.synth = new Tone.PolySynth(4, Tone.MonoSynth, {
       oscillator : {
-        type : "sawtooth"
+        type : "pulse",
+        width: 0.01
       },
       envelope : {
         attack : 0.00001 ,
         decay : 0.1 ,
         sustain : 0.3 ,
         release : 0.00001
+      },
+      filterEnvelope: {
+        attack : 0.06 ,
+        decay : 0.2 ,
+        sustain : 0.5 ,
+        release : 2 ,
+        baseFrequency: "A7",
+        octaves: 2.7
       }
     })
-    this.synth2 = new Tone.PolySynth(4, Tone.Synth, {
-      volume: 1,    
-      oscillator : {
-        type : "sawtooth"
-      },
-      envelope : {
-        attack : 0.00001 ,
-        decay : 0.1 ,
-        sustain : 0.2,
-        release : 0.00001
-      }
-    }).set("detune", 1200)
 
     this.filter = new Tone.Filter();
-    this.filter2 = new Tone.Filter();
     this.volume = new Tone.Gain();
-    this.volume2 = new Tone.Gain();
-    this.delay = new Tone.FeedbackDelay(0.2, 0.3).toMaster()
-    this.delay.connect(this.volume2)
 
     this.synth.connect(this.filter);
-    //this.synth2.connect(this.filter2);
     this.filter.connect(this.volume);
-    //this.filter.connect(this.delay);
-    this.filter2.connect(this.volume2);
     this.volume.toMaster();
-    this.volume2.toMaster();
     
     this.filter.frequency.value = 127 / 127 * 10800; // 200 - 15000
-    this.filter2.frequency.value = 30 / 127 * 10800; // 200 - 15000
     this.volume.gain.value = 127 / 127; // 0-0.8
-    this.volume2.gain.value = 6 / 127; // 0-0.8
   }
 
   toggleSound(type, input) {
     let method = type === 144 ? 'triggerAttack' : 'triggerRelease';
-    console.log('input : note', input, mapMIDINumberToTone(input + 12))
+    console.log('input : note', input, mapMIDINumberToTone(input + 12), method)
     this.synth[method](mapMIDINumberToTone(input + 12));
     //this.synth2[method](mapMIDINumberToTone(input));
   }
@@ -174,19 +165,33 @@ class Chimes {
     this.volume.gain.value = val;
   }
 
-  handleVolume2(value) { // 0-127
-    let val = value / 127; // Target: 28
-    this.volume2.gain.value = val;
-  }
-
   handleFilter(value) { // 0-127
     let val = value / 127 * 10800; // Target: 2
     this.filter.frequency.value = val;
   }
 
-  handleFilter2(value) { // 0-127
-    let val = value / 127 * 10800; // Target: 26
-    this.filter2.frequency.value = val;
+  handleAttack(value) {
+    let val = value / 1000
+    this.synth.voices[0].filterEnvelope.attack = val
+  }
+
+  handleDecay(value) {
+    let val = value / 1000
+    this.synth.voices[0].filterEnvelope.decay = val
+  }
+
+  handleSustain(value) {
+    let val = value / 127
+    this.synth.voices[0].filterEnvelope.sustain = val
+  }
+
+  handleRelease(value) {
+    let val = value / 100
+    this.synth.voices[0].filterEnvelope.release = val
+  }
+
+  handleBaseFrequency(value) {
+    this.synth.voices[0].filterEnvelope.baseFrequency = value
   }
 }
 
@@ -196,101 +201,146 @@ var started = false;
 document.documentElement.addEventListener('mousedown', () => {
   if (started) return;
   started = true;
-  const inst = new Chimes();
   const midi = new MIDIAccess({ onDeviceInput });
   midi.start().then(() => {
     console.log('STARTED!');
   }).catch(console.error);
 
-  document.onkeyup = e => {
-    console.log(e)
-  }
+  document.onkeydown = onKeyboardInput
 
+  document.onkeyup = onKeyboardInput
+  
+  function onKeyboardInput(e) {
+    const type = e.type === 'keydown' ? 144 : 123
+    switch (e.key) {
+      case 'a':
+        inst.toggleSound(type, 72)
+        break
+      case 's':
+        inst.toggleSound(type, 74)
+        break
+      case 'd':
+        inst.toggleSound(type, 76)
+        break
+      case 'f':
+        inst.toggleSound(type, 77)
+        break
+      case 'g':
+        inst.toggleSound(type, 79)
+        break
+      case 'h':
+        inst.toggleSound(type, 81)
+        break
+      case 'j':
+        inst.toggleSound(type, 83)
+        break
+      // case 'k':
+      //   inst.toggleSound(type, 84)
+      //   break
+    }
+  }
+  
+  // Baseline
+  // const inst = new Baseline();
+  // function onDeviceInput({ type, input, value }) {
+  //   if (input > 35 && input < 97) inst.toggleSound(type, input);
+  //   else if (input === 22) inst.handleVolume(value);
+  //   else if (input === 23) inst.handleVolume2(value);
+  //   else if (input === 26) inst.handleFilter(value);
+  //   else if (input === 27) inst.handleFilter2(value);
+  //   console.log('onDeviceInput!', type, input, value);
+  // }
+
+  // Chimes
+  const inst = new Chimes();
   function onDeviceInput({ type, input, value }) {
     if (input > 35 && input < 97) inst.toggleSound(type, input);
-    else if (input === 22) inst.handleVolume(value);
-    else if (input === 23) inst.handleVolume2(value);
-    else if (input === 26) inst.handleFilter(value);
-    else if (input === 27) inst.handleFilter2(value);
-    console.log('onDeviceInput!', type, input, value);
+    else if (input === 20) inst.handleVolume(value);
+    else if (input === 22) inst.handleAttack(value);
+    else if (input === 23) inst.handleDecay(value);
+    else if (input === 24) inst.handleSustain(value);
+    else if (input === 25) inst.handleRelease(value);
+    else if (input === 26) inst.handleBaseFrequency(value * 100);
+    else if (input === 27) inst.handleFilter(value);
+    else console.log('onDeviceInput!', type, input, value);
   }
 });
 
 
 function mapMIDINumberToTone (number) {
-    return {
-        "36": "C0",
-        "37": "C#0",
-        "38": "D0",
-        "39": "D#0",
-        "40": "E0",
-        "41": "F0",
-        "42": "F#0",
-        "43": "G0",
-        "44": "G#0",
-        "45": "A0",
-        "46": "A#0",
-        "47": "B0",
-        "48": "C1",
-        "49": "C#1",
-        "50": "D1",
-        "51": "D#1",
-        "52": "E1",
-        "53": "F1",
-        "54": "F#1",
-        "55": "G1",
-        "56": "G#1",
-        "57": "A1",
-        "58": "A#1",
-        "59": "B1",
-        "60": "C2",
-        "61": "C#2",
-        "62": "D2",
-        "63": "D#2",
-        "64": "E2",
-        "65": "F2",
-        "66": "F#2",
-        "67": "G2",
-        "68": "G#2",
-        "69": "A2",
-        "70": "A#2",
-        "71": "B2",
-        "72": "C3",
-        "73": "C#3",
-        "74": "D3",
-        "75": "D#3",
-        "76": "E3",
-        "77": "F3",
-        "78": "F#3",
-        "79": "G3",
-        "80": "G#3",
-        "81": "A3",
-        "82": "A#3",
-        "83": "B3",
-        "84": "C4",
-        "85": "C#4",
-        "86": "D4",
-        "87": "D#4",
-        "88": "E4",
-        "89": "F4",
-        "90": "F#4",
-        "91": "G4",
-        "92": "G#4",
-        "93": "A4",
-        "94": "A#4",
-        "95": "B4",
-        "96": "C5",
-        "97": "C#5",
-        "98": "D5",
-        "99": "D#5",
-        "100": "E5",
-        "101": "F5",
-        "102": "F#5",
-        "103": "G5",
-        "104": "G#5",
-        "105": "A5",
-        "106": "A#5",
-        "107": "B5",
-        "108": "C6"
-    }[number]
+  return [
+    "C0",
+    "C#0",
+    "D0",
+    "D#0",
+    "E0",
+    "F0",
+    "F#0",
+    "G0",
+    "G#0",
+    "A0",
+    "A#0",
+    "B0",
+    "C1",
+    "C#1",
+    "D1",
+    "D#1",
+    "E1",
+    "F1",
+    "F#1",
+    "G1",
+    "G#1",
+    "A1",
+    "A#1",
+    "B1",
+    "C2",
+    "C#2",
+    "D2",
+    "D#2",
+    "E2",
+    "F2",
+    "F#2",
+    "G2",
+    "G#2",
+    "A2",
+    "A#2",
+    "B2",
+    "C3",
+    "C#3",
+    "D3",
+    "D#3",
+    "E3",
+    "F3",
+    "F#3",
+    "G3",
+    "G#3",
+    "A3",
+    "A#3",
+    "B3",
+    "C4",
+    "C#4",
+    "D4",
+    "D#4",
+    "E4",
+    "F4",
+    "F#4",
+    "G4",
+    "G#4",
+    "A4",
+    "A#4",
+    "B4",
+    "C5",
+    "C#5",
+    "D5",
+    "D#5",
+    "E5",
+    "F5",
+    "F#5",
+    "G5",
+    "G#5",
+    "A5",
+    "A#5",
+    "B5",
+    "C6"
+  ][(number - 36) % 72]
 }
